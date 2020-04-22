a621386f049f621d589f89d07bcfdeaa6259bf1b
Aleksey Pleshkov <im@alekseypleshkov.ru>
2020-04-22 12:51:00 +0300

    Fix scale image in full screen slider

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -17,6 +17,8 @@ final class ViewController: UIViewController {
     super.viewDidLoad()
     
     sliderView.images = [
+      URL(string: "https://solutions.worldclass.ru/img/1/gyms/6371b9d6-ac8e-48df-882a-6f07253324e0.jpg")!,
+      URL(string: "https://solutions.worldclass.ru/img/1/gyms/b0f85cd8-4160-40e9-ba41-a4e0e922c8d6.jpg")!,
       URL(string: "https://images.unsplash.com/photo-1444076784383-69ff7bae1b0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80")!,
       URL(string: "https://images.unsplash.com/photo-1578664934514-789f4fb29160?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80")!,
       URL(string: "https://images.unsplash.com/photo-1578739207984-3514f3dfa64c?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80")!,
@@ -25,6 +27,7 @@ final class ViewController: UIViewController {
     sliderView.delegate = self
     sliderView.viewController = self
     sliderView.fullScreenBackButtonImage = UIImage(named: "backButton")
+//    sliderView.fullScreenImageContentMode = .scaleAspectFit
     sliderView.reloadData()
   }
 }
